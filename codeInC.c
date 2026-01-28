ae2fba635177f2b752ad115a487efd9efc9b0e82
durvesh <thes86088@gmail.com>
2026-01-29 00:32:23 +0530

    add bodies for paintX() and paintY()

--- codeInC.c
+++ codeInC.c
@@ -5,12 +5,14 @@ int canvas[size][size] = {0};
 
 void paintXAxis()
 {
-    
+     for( j=0; j<size; j++ )
+         canvas[mid][j] = 1;
 }
 
 void paintYAxis()
 {
-  
+     for( i=0; i<size; i++ )
+         canvas[i][mid] = 1;
 }
 
 void paintQuadOne()
