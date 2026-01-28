53238eb819f93e9732448c89309543acd1b3cd6f
durvesh <thes86088@gmail.com>
2026-01-29 00:51:32 +0530

    copy common functions/methods from 7segLED repo

--- codeInC.c
+++ codeInC.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+
 #define size 7
 int mid = size/2;
 int i=0, j=0;
-int canvas[size][size] = {0};
+int canvas[size][size] = {0}; int given =4;
 
-void paintXAxis()
+void paintXAxis()  
 {
      for( j=0; j<size; j++ )
          canvas[mid][j] = 1;
@@ -21,7 +23,7 @@ void paintQuadOne()
      {     
           for( j=size-1 ; j>mid; j-- )
           {
-               arr[i][j] = 1;
+               canvas[i][j] = 1;
           }      
      }
 }
@@ -32,7 +34,7 @@ void paintQuadTwo()
      {     
           for( j=0 ; j<mid; j++ )
           {
-               arr[i][j] = 1;
+               canvas[i][j] = 1;
           }      
      }
 }
@@ -43,7 +45,7 @@ void paintQuadThree()
      {     
           for( j=0 ; j<mid; j++ )
           {
-               arr[i][j] = 1;
+               canvas[i][j] = 1;
           }      
      } 
 }
@@ -54,14 +56,65 @@ void paintQuadFour()
      {     
           for( j=mid ; j<size-1; j++ )
           {
-               arr[i][j] = 1;
+               canvas[i][j] = 1;
           }      
+     }
+}
+
+void printMat()
+{
+    for( i=0; i<size; i++ )
+    {
+        for( j=0; j<size; j++ )
+        {
+            if( canvas[i][j] == 1 ) printf("*");
+            else if( canvas[i][j] == 0 ) printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+void initMat()
+{
+    for( i=0; i<size; i++ )
+    {
+        for( j=0; j<size; j++ )
+        {
+            canvas[i][j] = 0;
+        }
+    }
+}
+
+
+void paintMat()
+{
+    switch(given)
+    {
+        case 1 : paintQuadOne(); break;
+        case 2 : paintQuadTwo(); break;
+        case 3 : paintQuadThree(); break;
+        case 4 : paintQuadFour(); break;
+         
      }
 }
 
 int main()
 {
-  
+    initMat();
+    paintXAxis(); paintYAxis();
+    
+    do{
+        if( given>4 || given<1 )
+        {
+            printf("\n");
+            printf("invalid input! please try again !"); printf("\n");
+        }
+        printf("Enter a number 1-4 : ");
+        scanf( "%d", &given ); printf("\n");
+    }
+    while( given>4 || given<1 );
+    
+    paintMat();
+    printMat(); 
+    
 }
-
-
