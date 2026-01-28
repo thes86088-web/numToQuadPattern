93b42666f5880f0dc8a1ee8e57377ddbc66fe43c
durvesh <thes86088@gmail.com>
2026-01-29 00:28:49 +0530

    Create codeInC.c

---
+++ codeInC.c
@@ -0,0 +1,37 @@
+
+void paintXAxis()
+{
+  
+}
+
+void paintYAxis()
+{
+  
+}
+
+void paintQuadOne()
+{
+  
+}
+
+void paintQuadTwo()
+{
+  
+}
+
+void paintQuadThree()
+{
+  
+}
+
+void paintQuadFour()
+{
+  
+}
+
+int main()
+{
+  
+}
+
+
