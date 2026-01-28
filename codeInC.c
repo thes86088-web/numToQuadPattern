ce2dfbb331b4a777886b925b1c484386d58e10dc
durvesh <thes86088@gmail.com>
2026-01-29 00:42:18 +0530

    add function bodies for paintQaud() "i"

--- codeInC.c
+++ codeInC.c
@@ -17,22 +17,46 @@ void paintYAxis()
 
 void paintQuadOne()
 {
-  
+     for( i=0 ; i<mid; i++ )
+     {     
+          for( j=size-1 ; j>mid; j-- )
+          {
+               arr[i][j] = 1;
+          }      
+     }
 }
 
 void paintQuadTwo()
 {
-  
+  for( i=0 ; i<mid; i++ )
+     {     
+          for( j=0 ; j<mid; j++ )
+          {
+               arr[i][j] = 1;
+          }      
+     }
 }
 
 void paintQuadThree()
 {
-  
+     for( i=mid ; i<size-1; i++ )
+     {     
+          for( j=0 ; j<mid; j++ )
+          {
+               arr[i][j] = 1;
+          }      
+     } 
 }
 
 void paintQuadFour()
 {
-  
+     for( i=mid ; i<size-1; i++ )
+     {     
+          for( j=mid ; j<size-1; j++ )
+          {
+               arr[i][j] = 1;
+          }      
+     }
 }
 
 int main()
